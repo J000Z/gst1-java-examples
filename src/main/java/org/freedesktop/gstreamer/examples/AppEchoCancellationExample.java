dd859f9ad94fbbba57b2aacb44c73ef05b0a4cd6
Jack Z <jack@vowel.com>
2019-09-02 19:12:11 +0800

    multi channel aec

--- src/main/java/org/freedesktop/gstreamer/examples/AppEchoCancellationExample.java
+++ src/main/java/org/freedesktop/gstreamer/examples/AppEchoCancellationExample.java
@@ -12,6 +12,9 @@
 package org.freedesktop.gstreamer.examples;
 
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Streams;
+import org.apache.commons.io.IOUtils;
 import org.freedesktop.gstreamer.*;
 import org.freedesktop.gstreamer.elements.AppSink;
 import org.freedesktop.gstreamer.elements.AppSrc;
@@ -20,27 +23,18 @@ import org.jcodec.codecs.wav.WavHeader;
 
 import java.io.*;
 import java.nio.ByteBuffer;
-import java.nio.channels.Channel;
-import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-/**
- *
- * A simple example of transcoding an audio file using AppSink and AppSrc.
- *
- * Depending on which gstreamer plugins are installed, this example can decode
- * nearly any media type, likewise for encoding. Specify the input and output
- * file locations with the first two arguments.
- *
- * @author Kyle R Wenholz (http://krwenholz.com)
- */
 public class AppEchoCancellationExample {
-
-    private static Pipeline pipe;
 
     static byte[] readAudioFile(String path) throws IOException {
         File file = new File(path);
@@ -53,10 +47,11 @@ public class AppEchoCancellationExample {
         source.set("emit-signals", true);
         source.connect(new AppSrc.NEED_DATA() {
 
-            private final ByteBuffer bb = ByteBuffer.wrap(bytes);
+            ByteBuffer bb = ByteBuffer.wrap(bytes);
 
             @Override
             public void needData(AppSrc elem, int size) {
+                // infinite repeating
                 if (bb.hasRemaining()) {
                     System.out.println("needData: size = " + size);
                     byte[] tempBuffer;
@@ -133,43 +128,55 @@ public class AppEchoCancellationExample {
         });
     }
 
+    static Element createAppSrc(Pipeline pipeline, String name, byte[] data) {
+        AppSrc appSrc = (AppSrc) makeElement(pipeline, "appsrc", name);
+        Element decoder = makeElement(pipeline, "decodebin", String.format("%s-decoder", name));
+        Element converter = makeElement(pipeline, "audioconvert", String.format("%s-speaker-converter", name));
+        setupAppSource(appSrc, data);
+        appSrc.link(decoder);
+        dynamicLinkDecoderAndConverter(decoder, converter);
+        return converter;
+    }
+
+    static Element makeElement(Pipeline pipeline, String factoryName, String name) {
+        Element element = ElementFactory.make(factoryName, name);
+        pipeline.add(element);
+        return element;
+    }
+
+    static Element link(Element ...elements) {
+        for (int i=0; i<elements.length-1; i++) {
+            elements[i].link(elements[i+1]);
+        }
+        return elements[elements.length-1];
+    }
+
+    static boolean outputToSpeaker = false;
+
     public static void main(String[] args) throws Exception {
         Gst.init();
 
         Registry.get().scanPath("/usr/local/lib/gstreamer-1.0/");
 
-//        String speakerOutputLocation = "/home/ubuntu/vowel/resources/noodle.pcm";
-        byte[] speakerData = Files.readAllBytes(Paths.get("/home/ubuntu/vowel/resources/noodle.wav"));
-        byte[] microphoneData = Files.readAllBytes(Paths.get("/home/ubuntu/vowel/resources/out-without-aec.wav"));
+        List<byte[]> remoteParticipants = IntStream.range(0, 4)
+                .mapToObj(i -> String.format("/amicorpus/ES2002a/audio/ES2002a.Headset-%d.wav", i))
+                .map(location -> {
+                    try (InputStream inputStream = AppEchoCancellationExample.class.getResourceAsStream(location)) {
+                        return IOUtils.toByteArray(inputStream);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        throw new IllegalStateException();
+                    }
+                }).collect(Collectors.toList());
 
+        byte[] localParticipant = Files.readAllBytes(Paths.get("/home/ubuntu/vowel/resources/noodle.wav"));
+
+//        String speakerOutputLocation = "/home/ubuntu/vowel/resources/noodle.pcm";
+//        byte[] microphoneData = Files.readAllBytes(Paths.get("/home/ubuntu/vowel/resources/out-without-aec.wav"));
 //        byte[] speakerData = readAudioFile("/home/ubuntu/vowel/resources/noodle.wav");
 //        byte[] microphoneData = readAudioFile("/home/ubuntu/vowel/resources/out-without-aec.wav");
 
         final MainLoop loop = new MainLoop();
-
-
-        AppSrc speakerSource = (AppSrc)ElementFactory.make("appsrc", "speaker-source");
-        Element speakerDecoder = ElementFactory.make("decodebin", "speaker-decoder");
-        Element speakerConverter = ElementFactory.make("audioconvert", "speaker-converter");
-        Element echoProbe = ElementFactory.make("webrtcechoprobe", "echo-probe");
-        Element probeVolume = ElementFactory.make("volume", "probe-volume");
-        probeVolume.set("volume", 0);
-//        AppSink fakeSink = (AppSink)ElementFactory.make("appsink", "fake-sink");
-
-        AppSrc microphoneSource = (AppSrc)ElementFactory.make("appsrc", "microphone-source");
-        Element microphoneDecoder = ElementFactory.make("decodebin", "microphone-decoder");
-        Element microphoneConverter = ElementFactory.make("audioconvert", "microphone-converter");
-        Element echoDsp = ElementFactory.make("webrtcdsp", "echo-dsp");
-        echoDsp.set("echo-cancel", true);
-        echoDsp.set("echo-suppression-level", 1);
-        echoDsp.set("probe", echoProbe.getName());
-
-        Element mixerSink = ElementFactory.make("audiomixer", "audio-mixer");
-        Element speakerSink = ElementFactory.make("autoaudiosink", "speaker-sink");
-
-//        Caps srcCaps = Caps.fromString("audio/x-raw,rate=48000,channels=1,format=S16LE");
-//        speakerSource.setCaps(srcCaps);
-//        microphoneSource.setCaps(srcCaps);
 
         Pipeline pipe = new Pipeline();
         // We connect to EOS and ERROR on the bus for cleanup and error messages.
@@ -187,31 +194,81 @@ public class AppEchoCancellationExample {
             loop.quit();
         });
 
-        setupAppSource(speakerSource, speakerData);
-        setupAppSource(microphoneSource, microphoneData);
-//        setupAppSink(fakeSink, speakerOutputLocation);
+        List<Element> remoteParticipantTees = Streams
+                .mapWithIndex(remoteParticipants.stream(), (data, idx) -> {
+                    Element appSrc = createAppSrc(pipe, String.format("remote-participant-src-%d", idx), data);
+                    // keep sample rate the same
+                    Element resample = makeElement(pipe, "audioresample", String.format("resample-%d", idx));
+                    Caps caps = Caps.fromString("audio/x-raw, rate=48000");
+                    // this ami data has very low volume
+                    Element amplifier = makeElement(pipe, "audioamplify", String.format("audioamplify-%d", idx));
+                    amplifier.set("amplification", 5.0);
+                    Element tee = makeElement(pipe, "tee", String.format("remote-participant-tee-%d", idx));
+                    appSrc.link(resample);
+                    resample.linkFiltered(amplifier, caps);
+                    link(amplifier, tee);
+                    return tee;
+                })
+                .collect(Collectors.toList());
 
-        pipe.addMany(
-                speakerSource, speakerDecoder, speakerConverter, echoProbe, probeVolume,
-                microphoneSource, microphoneDecoder, microphoneConverter, echoDsp,
-                mixerSink, speakerSink);
+        // mix participants & delay
+        Element remoteParticipantsMixer = makeElement(pipe, "audiomixer", "remote-participants-mixer");
+        Streams.mapWithIndex(remoteParticipantTees.stream(), (tee, idx) -> {
+            Element queue = makeElement(pipe, "queue", String.format("mix-queue-%d", idx));
+            tee.link(queue);
+            return queue;
+        }).forEach(queue -> queue.link(remoteParticipantsMixer));
+        Element remoteParticipantsMixerDelay = makeElement(pipe, "entransshift", "participants-mixer-delay");
+        remoteParticipantsMixerDelay.set("delay", 500);
+        remoteParticipantsMixerDelay.set("running-time", false);
+        remoteParticipantsMixer.link(remoteParticipantsMixerDelay);
+
+        // create simulated microphone input
+        Element localParticipantSource = createAppSrc(pipe, "local-participant", localParticipant);
+        Element localParticipantVolume = makeElement(pipe, "volume", "local-participant-volume");
+        localParticipantVolume.set("volume", 0.3); // noodle is too loud
+        Element simulateMicrophoneMixer = makeElement(pipe, "audiomixer", "simulated-microphone-mixer");
+        remoteParticipantsMixerDelay.link(simulateMicrophoneMixer);
+        link(localParticipantSource, localParticipantVolume, simulateMicrophoneMixer);
 
         // probe
-        speakerSource.link(speakerDecoder);
-        dynamicLinkDecoderAndConverter(speakerDecoder, speakerConverter);
-        speakerConverter.link(echoProbe);
-        echoProbe.link(probeVolume);
+        List<Element> probes = Streams.mapWithIndex(remoteParticipantTees.stream(), (tee, idx) -> {
+            // keep sample rate the same
+            Element queue = makeElement(pipe, "queue", String.format("queue-sync-%d", idx));
+            Element probe = makeElement(pipe, "webrtcechoprobe", String.format("echo-probe-%d", idx));
+            return link(tee, queue, probe);
+        }).collect(Collectors.toList());
 
-        // echo cancellation
-        microphoneSource.link(microphoneDecoder);
-        dynamicLinkDecoderAndConverter(microphoneDecoder, microphoneConverter);
-        microphoneConverter.link(echoDsp);
+        // add webrtc dsp
+        List<Element> dsps = Streams.mapWithIndex(probes.stream(), (probe, idx) -> {
+            Element echoDsp = makeElement(pipe, "webrtcdsp", String.format("echo-dsp-%d", idx));
+            echoDsp.set("echo-cancel", true);
+            echoDsp.set("echo-suppression-level", 1);
+            echoDsp.set("probe", probe.getName());
+            return echoDsp;
+        }).collect(Collectors.toList());
+        Element dspResult = link(ImmutableList.builder().add(simulateMicrophoneMixer).addAll(dsps).build().toArray(Element[]::new));
 
-        // mixer (aec need it works, maybe for synchronization purpose)
-        probeVolume.link(mixerSink);
-        echoDsp.link(mixerSink);
-        mixerSink.link(speakerSink);
+        Element finalMixer = makeElement(pipe, "audiomixer", "final-mixer");
+        Streams.mapWithIndex(probes.stream(), (probe, idx) -> {
+            Element volume = makeElement(pipe, "volume", String.format("volume-%d", idx));
+            volume.set("volume", 0);
+            return link(probe, volume);
+        }).forEach(muted -> muted.link(finalMixer));
+        dspResult.link(finalMixer);
 
+        if (outputToSpeaker) {
+            Element speakerSink = makeElement(pipe, "autoaudiosink", "speaker-sink");
+            finalMixer.link(speakerSink);
+        } else {
+            Element encoder = makeElement(pipe, "wavenc", "wav enc");
+            AppSink appSink = (AppSink)makeElement(pipe, "appsink", "audio-output");
+            appSink.set("sync", false);
+            setupAppSink(appSink, "out.wav");
+            link(finalMixer, encoder, appSink);
+        }
+
+        long start = System.currentTimeMillis();
 
         System.out.println("Playing...");
         pipe.play();
@@ -221,6 +278,9 @@ public class AppEchoCancellationExample {
         pipe.stop();
         Gst.deinit();
         Gst.quit();
+
+        long end = System.currentTimeMillis();
+        System.out.println(String.format("Runtime: %d", end - start));
 
     }
 }
